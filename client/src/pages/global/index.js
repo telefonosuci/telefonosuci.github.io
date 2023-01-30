eaff550dc210bc1f301663500c54bdb0acf64d69
Enrico <telefonosuci@gmail.com>
2023-01-30 11:16:49 +0100

    Added log on global entrypoint

--- client/src/pages/global/index.js
+++ client/src/pages/global/index.js
@@ -1,5 +1,6 @@
 import LogRocket from 'logrocket';
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('Loaded global entry point');
   LogRocket.init('1k2gse/telefonosuci-site');
 });
