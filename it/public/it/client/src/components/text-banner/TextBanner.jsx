84cf2ec81efc94defb604009020bfdc9cf79537a
Enrico <telefonosuci@gmail.com>
2023-02-06 20:30:26 +0100

    Updates

--- it/public/it/client/src/components/text-banner/TextBanner.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TextBanner({props}) {
-  return <div>Hi, I am a Banner!</div>;
-}
-
-export default TextBanner;
--- public/it/client/webpack/aliases.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const rootPath = require("./rootPath").root;
-
-console.log("Root path is: ", rootPath);
-module.exports = {
-  "@": path.resolve(rootPath, "src"),
-  "@App": path.resolve(rootPath, "app"),
-};
