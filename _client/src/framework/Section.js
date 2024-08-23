3ebd419807a33d8c9e0e2c045ed056b83135ed71
SUCI <telefonosuci@gmail.com>
2024-08-24 00:24:51 +0200

    some changes

---
+++ _client/src/framework/Section.js
@@ -0,0 +1,11 @@
+class Section {
+  constructor(selector) {
+    this.selector = selector;
+  }
+  /**
+     * Implementation required
+     */
+  render() {
+    throw new Error('You have to implement the method doSomething!');
+  }
+}
